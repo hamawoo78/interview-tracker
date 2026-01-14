9d0791f3f94186afd9a6f16beba30b1579e34587
hamawoo78 <nae.yk.jp@gmail.com>
2026-01-13 21:33:08 -0800

    Update settings

--- interview_tracker/settings.py
+++ interview_tracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key-for-dev-only')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['interview-tracker-52v8.onrender.com']
 
 
 # Application definition
